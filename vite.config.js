4c74c2105a8b12802da8544f791b5a3fa74bf02d
Mihir Dudhatra <mihirdudhatra3906@gmail.com>
2024-08-27 11:56:10 +0530

    Hosted

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/EduWeb/', 
-  plugins: [react()]
+  plugins: [react()],
+  base: '/EduWeb/',
+  build: {
+    outDir: 'dist'
+  }
 });
